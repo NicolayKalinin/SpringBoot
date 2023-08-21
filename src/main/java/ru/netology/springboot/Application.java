73c924052267e08c3e783876d1046f2b1c5f186f
Nicolay Kalinin <nikolajk202@gmail.com>
2023-08-21 20:26:48 +0300

    Files added and modified

--- src/main/java/ru/netology/springboot/Application.java
+++ src/main/java/ru/netology/springboot/Application.java
@@ -2,6 +2,11 @@ package ru.netology.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +15,29 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@ConditionalOnProperty
+	@Bean
+	public SystemProfile devProfile() {
+		return new DevProfile();
+	}
+
+	@Bean
+	public SystemProfile prodProfile() {
+		return new ProductionProfile();
+	}
+
+	@RestController
+	@RequestMapping("/")
+	public static class ProfileController {
+		private final SystemProfile profile;
+
+		public ProfileController(SystemProfile profile) {
+			this.profile = profile;
+		}
+
+		@GetMapping("profile")
+		public String getProfile() {
+			return profile.getProfile();
+		}
+	}
 }
